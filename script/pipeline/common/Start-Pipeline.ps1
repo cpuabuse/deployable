e1b8a1d9cd7e7883706792b76576f79ae17a10fe
Herman <35882542+winterhotlatte@users.noreply.github.com>
2019-12-27 15:51:35 +0900

    Add: Restoration of error action in pipelines

--- script/pipeline/common/Start-Pipeline.ps1
+++ script/pipeline/common/Start-Pipeline.ps1
@@ -1,13 +1,17 @@
 # File is to be dot-sourced
 
-$ErrorActionPreference = "Stop"
-
 if ($null -eq (Get-Variable -Name "StartPipeline" -Scope "Script" -ErrorAction "Ignore")) {
 	# Guard
 	New-Variable -Name "StartPipeline" -Scope "Script"
 
+	# Set the error action
+	$ErrorActionPreferenceOriginal = $ErrorActionPreference
+	$ErrorActionPreference = "Stop"
+
+	# Change path to repo root
 	Push-Location -Path $(Join-Path -Path $PSScriptRoot -ChildPath ".." ".." "..")
 
+	# Create paths hashtable
 	[ValidateNotNull()][hashtable]$script:Paths += @{
 		DeployCoverage      = Join-Path -Path "script" -ChildPath "pipeline" "common" "Deploy-Coverage.ps1";
 		InstallDependencies = Join-Path -Path "script" -ChildPath "pipeline" "common" "Install-Dependencies.ps1";
--- script/pipeline/common/Stop-Pipeline.ps1
+++ script/pipeline/common/Stop-Pipeline.ps1
@@ -1,6 +1,10 @@
 # File is to be dot-sourced
 
 if ($null -ne (Get-Variable -Name "StartPipeline" -Scope "Script" -ErrorAction "Ignore")) {
+	# Revert error action
+	$ErrorActionPreference = $script:ErrorActionPreferenceOriginal
+	Remove-Variable -Name "ErrorActionPreferenceOriginal" -Scope "Script"
+
 	# Remove Paths var
 	Remove-Variable -Name "Paths" -Scope "Script"
 	
